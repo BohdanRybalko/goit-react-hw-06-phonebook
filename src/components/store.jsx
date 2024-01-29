63c06af149610f27a23107841b199922f9331f1a
BohdanRybalko <147001546+BohdanRybalko@users.noreply.github.com>
2024-01-29 17:16:40 +0200

    fix store

--- src/components/store.jsx
+++ src/components/store.jsx
@@ -1,17 +1,29 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore} from '@reduxjs/toolkit';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import contactsReducer from '../redux/contactsSlice';
+import logger from 'redux-logger';
 
 const persistConfig = {
   key: 'root',
   storage,
+  blacklist: ['filter'],
 };
 
 const persistedReducer = persistReducer(persistConfig, contactsReducer);
 
-export const store = configureStore({
+const ignoreActions = ['FLUSH', 'REHYDRATE', 'PAUSE', 'PERSIST', 'PURGE', 'REGISTER'];
+
+const store = configureStore({
   reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ignoreActions.map((actionType) => `persist/${actionType}`),
+      },
+    }).concat(logger),
 });
 
 export const persistor = persistStore(store);
+
+export default store;
